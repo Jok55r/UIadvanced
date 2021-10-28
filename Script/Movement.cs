1172f75c07e614283b3530acc0c8bf78c8e03a8f
Jok55r <81425160+Jok55r@users.noreply.github.com>
2021-10-28 17:44:37 +0200

    test

---
+++ Script/Movement.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 100f;
+    public float JumpHeight;
+    public bool InAir = false;
+
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        InAir = false;
+        Debug.Log("InAir false");
+    }
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        InAir = true;
+        Debug.Log("InAir True");
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 NoMovement = new Vector2(0f, 0f);
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        if (moveHorizontal > 0)
+        {
+            {
+                rb2d.velocity = new Vector2(speed, rb2d.velocity.y);
+
+            }
+        }
+        if (moveHorizontal < 0)
+        {
+            rb2d.velocity = new Vector2(-speed, rb2d.velocity.y);
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            if (InAir == false)
+            {
+                rb2d.AddForce(new Vector2(0, JumpHeight), ForceMode2D.Impulse);
+            }
+        }
+    }
+
+}
